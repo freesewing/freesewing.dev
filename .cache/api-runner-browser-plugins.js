56e4fa282c39ae37c6f280c0a85d9ad6b4de43e7
Joost De Cock <joost@decock.org>
2019-05-12 12:37:12 +0200

    :fire: Removed public and .cache from repo

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/home/EP/jdecock/git/fs/devsite/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -20,7 +13,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
