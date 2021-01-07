48d26fcbf3ac63904389d66c9612ba94cfed4ecd
Joost De Cock <joost@joost.at>
2021-01-07 18:09:32 +0100

    feat: Updated to fluid layout

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -5,20 +5,22 @@ import './default.scss'
 
 const DefaultLayout = (props) => {
   return (
-    <div className="layout" dataLayout="docs">
-      <aside>
-        <div className="sticky">
-          {props.mainMenu}
-        </div>
-      </aside>
-      <section>
-        {!props.noCrumbs && <BreadCrumbs crumbs={props.crumbs} pageTitle={props.title} />}
-        {!props.noTitle && <h1>{props.title}</h1>}
-        <div className={`content ${props.wide ? 'wide' : ''}`}>
-          {props.children}
-          <PrevNext prev={props.prev} next={props.next} />
-        </div>
-      </section>
+    <div className="layout-wrapper">
+      <div className="layout" dataLayout="docs">
+        <aside>
+          <div className="sticky">
+            {props.mainMenu}
+          </div>
+        </aside>
+        <section>
+          {!props.noCrumbs && <BreadCrumbs crumbs={props.crumbs} pageTitle={props.title} />}
+          {!props.noTitle && <h1>{props.title}</h1>}
+          <div className={`content ${props.wide ? 'wide' : ''}`}>
+            {props.children}
+            <PrevNext prev={props.prev} next={props.next} />
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
